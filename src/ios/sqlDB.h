86cd04a6ed6dc1b069630666c3f295e5b665b489
Rahul Pandey <rahul.p@applicationnexus.com>
2017-03-10 17:50:03 +0530

    Add checkDbOnStorage Method

--- src/ios/sqlDB.h
+++ src/ios/sqlDB.h
@@ -14,10 +14,10 @@
 
 @interface sqlDB : CDVPlugin
 {
-    NSFileManager* fileManager;
-    NSString* dbPath;
-    NSString* documentsDirectory;
-    NSString* libraryDirectory;
+	NSFileManager* fileManager;
+	NSString* dbPath;
+	NSString* documentsDirectory;
+	NSString* libraryDirectory;
 }
 
 - (void)copy:(CDVInvokedUrlCommand*)command;
@@ -27,8 +27,9 @@
 - (void) copyDbToStorage:(CDVInvokedUrlCommand *) command;
 
 - (void) copyDbFromStorage:(CDVInvokedUrlCommand *)command;
-	
 
--(void)sendPluginResponse:(NSInteger*)code msg:(NSString*)msg err:(BOOL)error callBackID:(NSString*)cid;
+- (void)checkDbOnStorage : (CDVInvokedUrlCommand *) command;
+
+- (void)sendPluginResponse:(NSInteger*)code msg:(NSString*)msg err:(BOOL)error callBackID:(NSString*)cid;
 
 @end
